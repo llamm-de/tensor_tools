78a4cebc0f842c875799748889626c08e67ea70e
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2020-07-10 14:59:32 +0200

    Add getGreenLagrange to libtt_tensors_physics

--- src/libtt_tensors_physics.f90
+++ src/libtt_tensors_physics.f90
@@ -1,11 +1,14 @@
 !> Module for special tensors used in pyhsical simulations (e.g. solid mechanics)
 module libtt_tensors_physics
 
+    use libtt_tensors_common
+
     implicit none
     private
 
     public :: getRightCauchyGreen
     public :: getLeftCauchyGreen
+    public :: getGreenLagrange
     
 contains
 
@@ -34,5 +37,18 @@ contains
         res = matmul(defGrad, transpose(defGrad))
 
     end function getLeftCauchyGreen
+
+    !> Green Lagrange strain tensor
+    !!
+    !! @param  defGrad Deformation gradient tensor
+    !! @return res     Green Lagrange strain tensor
+    pure function getGreenLagrange(defGrad) result(res)
+
+        real(kind=8), dimension(3,3), intent(in) :: defGrad
+        real(kind=8), dimension(3,3)             :: res
+
+        res = 0.5d0*(getRightCauchyGreen(defGrad) - eye())
+
+    end function getGreenLagrange
 
 end module libtt_tensors_physics
