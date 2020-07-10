f1eb8555b8f20ff27fb0f82bfe8c2c07dcb50477
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2020-07-10 17:54:05 +0200

    Add inverse() to libtt_tensors_common

--- src/libtt_tensors_common.f90
+++ src/libtt_tensors_common.f90
@@ -1,5 +1,7 @@
 !> Module for common tensors
 module libtt_tensors_common
+    
+    use libtt_common, only: det
 
     implicit none
     private
@@ -7,6 +9,7 @@ module libtt_tensors_common
     public :: eye
     public :: symmetric
     public :: scew
+    public :: inverse
 
 contains
 
@@ -44,7 +47,27 @@ contains
         real(kind=8), dimension(3,3)             :: res
 
         res = 0.5d0 * (A - transpose(A))
-        
+
     end function scew
+
+    !> Inverse of 2nd order tensor
+    !! 
+    !! @param  A   Tensor of second order
+    !! @return res Inverse of A
+    pure function inverse(A) result(res)
+        real(kind=8), dimension(3,3), intent(in) :: A
+        real(kind=8), dimension(3,3)             :: res
+
+        res(1,1) = A(2,2)*A(3,3) - A(2,3)*A(3,2)
+        res(2,1) = A(2,3)*A(3,1) - A(3,3)*A(2,1)
+        res(3,1) = A(2,1)*A(3,2) - A(3,1)*A(2,2)
+        res(1,2) = A(3,2)*A(1,3) - A(1,2)*A(3,3)
+        res(2,2) = A(3,3)*A(1,1) - A(1,3)*A(3,1)
+        res(3,2) = A(3,1)*A(1,2) - A(1,1)*A(3,2)
+        res(1,3) = A(1,2)*A(2,3) - A(2,2)*A(1,3)
+        res(2,3) = A(1,3)*A(2,1) - A(2,3)*A(1,1)
+        res(3,3) = A(1,1)*A(2,2) - A(2,1)*A(1,2)
+        res      = 1/det(A) * res
+    end function inverse
 
 end module libtt_tensors_common
