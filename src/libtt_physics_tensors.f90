6543f8af20b49fb50d14fa409b9f5f69e8d64b17
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2020-07-10 12:06:39 +0200

    Add @before method to set up defGrad in test_physics_tensors

--- src/libtt_physics_tensors.f90
+++ src/libtt_physics_tensors.f90
@@ -1,3 +1,4 @@
+!> Module for special tensors used in pyhsical simulations (e.g. solid mechanics)
 module libtt_physics_tensors
 
     implicit none
@@ -8,6 +9,10 @@ module libtt_physics_tensors
     
 contains
 
+    !> Right Cauchy Green tensor
+    !!
+    !! @param  defGrad Deformation gradient tensor
+    !! @return res     Right Cauchy Green tensor 
     pure function getRightCauchyGreen(defGrad) result(res)
 
         real(kind=8), dimension(3,3), intent(in) :: defGrad
@@ -17,7 +22,10 @@ contains
 
     end function getRightCauchyGreen
 
-
+    !> Left Cauchy Green tensor
+    !!
+    !! @param  defGrad Deformation gradient tensor
+    !! @return res     Left Cauchy Green tensor 
     pure function getLeftCauchyGreen(defGrad) result(res)
 
         real(kind=8), dimension(3,3), intent(in) :: defGrad
