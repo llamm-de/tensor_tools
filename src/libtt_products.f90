67faf57ed609ff071524177fa1e19bf8226936a1
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2020-07-13 16:55:43 +0200

    Introduce .ddot. operator for double contraction and change products to pure functions

--- src/libtt_products.f90
+++ src/libtt_products.f90
@@ -5,6 +5,15 @@ module libtt_products
     private
 
     public :: doubleContract
+    public :: operator(.ddot.)
+    
+    !> Double contracting (inner) product A:B for tensors of various
+    !! ranks. 
+    interface operator(.ddot.)
+        module procedure double_contract_ranks22
+        module procedure double_contract_ranks24    
+        module procedure double_contract_ranks42 
+    end interface
     
     !> Double contracting (inner) product A:B for tensors of various
     !! ranks. 
@@ -20,28 +29,28 @@ contains
     !! @param[in]  a      First tensor of product A:B
     !! @param[in]  b      Second tensor of product A:B
     !! @param[out] result Scalar result of product A:B
-    subroutine double_contract_ranks22(result, a, b)
-        real(kind=8)                              :: result        
+    pure function double_contract_ranks22(a, b) result(res)
+        real(kind=8)                              :: res        
         real(kind=8), dimension (3,3), intent(in) :: a, b
         
         integer :: i,j
     
-        result = 0
+        res = 0
         do i = 1,3,1
           do j = 1,3,1
-            result = result + a(i,j)*b(i,j)
+            res = res + a(i,j)*b(i,j)
           end do
         end do
-    end subroutine double_contract_ranks22
+    end function double_contract_ranks22
 
     !> Double contracting product for tensors of rank 2 and rank 4
     !! @param[in]  a      First tensor (rank 2) of product A:B
     !! @param[in]  b      Second tensor (rank 4) of product A:B
     !! @param[out] result Rank 2 tensor result of product A:B
-    subroutine double_contract_ranks24(result, a, b)
+    pure function double_contract_ranks24(a, b) result(res)
         real(kind=8), dimension(3,3,3,3), intent(in) :: b
         real(kind=8), dimension(3,3), intent(in)     :: a
-        real(kind=8), dimension(3,3)                 ::result
+        real(kind=8), dimension(3,3)                 ::res
         
         integer :: i, j, m, n
 
@@ -49,21 +58,21 @@ contains
             do n = 1,3,1
                 do i = 1,3,1
                     do j = 1,3,1
-                        result(m,n) = result(m,n) + a(i,j)*b(i,j,m,n)
+                        res(m,n) = res(m,n) + a(i,j)*b(i,j,m,n)
                     end do
                 end do
             end do
         end do
-    end subroutine double_contract_ranks24
+    end function double_contract_ranks24
 
     !> Double contracting product for tensors of rank 4 and rank 2
     !! @param[in]  a      First tensor (rank 4) of product A:B
     !! @param[in]  b      Second tensor (rank 2) of product A:B
     !! @param[out] result Rank 2 tensor result of product A:B
-    subroutine double_contract_ranks42(result, a, b)
+    pure function double_contract_ranks42(a, b) result(res)
         real(kind=8), dimension(3,3,3,3), intent(in) :: a
         real(kind=8), dimension(3,3), intent(in)     :: b
-        real(kind=8), dimension(3,3)                 ::result
+        real(kind=8), dimension(3,3)                 :: res
         
         integer :: i, j, k, l
 
@@ -71,11 +80,11 @@ contains
             do j = 1,3,1
                 do k = 1,3,1
                     do l = 1,3,1
-                        result(i,j) = result(i,j) + a(i,j,k,l)*b(k,l)
+                        res(i,j) = res(i,j) + a(i,j,k,l)*b(k,l)
                     end do
                 end do
             end do
         end do
-    end subroutine double_contract_ranks42
+    end function double_contract_ranks42
 
 end module libtt_products
