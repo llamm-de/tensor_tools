514fe57b434c0b77e9710d045af53017007d62a2
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2020-07-14 14:46:33 +0200

    Add toTensor() to libtt_voigt

--- src/libtt_voigt.f90
+++ src/libtt_voigt.f90
@@ -1,12 +1,15 @@
 module libtt_voigt
     
-    use libtt_precision, only: dp
+    use libtt_precision,      only: dp
+    use libtt_tensors_common, only: symmetric
 
     implicit none
     private
 
     public :: toVoigt
     public :: toVoigtNonSym
+    public :: toTensor
+    public :: toTensorNonSym
 
     interface toVoigt
         module procedure toVoigt2nd
@@ -16,6 +19,13 @@ module libtt_voigt
         module procedure toVoigt2nd_nonSym
     end interface toVoigtNonSym
 
+    interface toTensor
+        module procedure toTensor2nd
+    end interface toTensor
+
+    interface toTensorNonSym
+        module procedure toTensor2nd_nonSym
+    end interface toTensorNonSym
 contains
 
     pure function toVoigt2nd_nonSym(a) result(res)
@@ -46,6 +56,38 @@ contains
         res(6) = a(1,3)
                 
     end function toVoigt2nd
+
+    pure function toTensor2nd_nonSym(a) result(res)
+        real(kind=dp), dimension(9), intent(in) :: a
+        real(kind=dp), dimension(3,3)           :: res
+
+        res(1,1) = a(1)
+        res(1,2) = a(2)
+        res(1,3) = a(3)
+        res(2,1) = a(4)
+        res(2,2) = a(5)
+        res(2,3) = a(6)
+        res(3,1) = a(7)
+        res(3,2) = a(8)
+        res(3,3) = a(9)
+
+    end function toTensor2nd_nonSym
+
+    pure function toTensor2nd(a) result(res)
+        real(kind=dp), dimension(6), intent(in) :: a
+        real(kind=dp), dimension(3,3)           :: res
+
+        res(1,1) = a(1)
+        res(2,2) = a(2)
+        res(3,3) = a(3)
+        res(1,2) = a(4)
+        res(2,3) = a(5)
+        res(1,3) = a(6)
+        res(2,1) = res(1,2)        
+        res(3,2) = res(2,3)        
+        res(3,1) = res(1,3)        
+
+    end function toTensor2nd
 
 end module libtt_voigt
     
