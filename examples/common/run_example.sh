83327a850fbb94d8820149204935c219a62cea30
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2020-07-27 14:50:28 +0200

    Completed examples for libtt_common

--- examples/common/run_example.sh
+++ examples/common/run_example.sh
@@ -2,15 +2,17 @@
 green=$'\e[1;32m'
 end=$'\e[0m'
 
-if [ ! -f ./common_example ]; then
+if [ ! -f ./common_example.out ]; then
     printf  "%s\n" "${green} Building example for common operations. ${end}"
     make
-    printf  "%s\n" "${green} Removing temporary build files. ${end}"
-    rm *.mod *.o
 else
     printf  "%s\n" "${green} Executable already exists. No build needed. ${end}"
 fi
 
 printf  "%s\n" "${green} Executing example. ${end}"
-./common_example
+./common_example.out
+
+# Clean up
+printf  "%s\n" "${green} Removing build files. ${end}"
+make clean
 
