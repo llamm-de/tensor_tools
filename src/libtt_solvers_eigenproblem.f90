079638fffc1f09fa20c9367161994be993ac8c40
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2020-08-04 14:27:51 +0200

    Add jacobi() solver to libtt_solvers_eigenproblem

--- src/libtt_solvers_eigenproblem.f90
+++ src/libtt_solvers_eigenproblem.f90
@@ -7,6 +7,7 @@ module libtt_solvers_eigenproblem
     private
     
     public :: powerMethod
+    public :: jacobi
     
 contains
 
@@ -59,11 +60,94 @@ contains
 
         ! Error handling
         if (iter == maxIter .AND. delta >= tolerance) then
-            error stop "ERROR: PowerMethod did not converge!"
+            error stop "ERROR in POWERMETHOD: Solution did not converge!"
         end if
 
         deallocate(y)
 
     end subroutine
+
+    !> Jacobi method to compute eigenvalues/-vectors for symmetric tensors/matrices
+    !! Results are not sorted!
+    !!
+    !! @param A      Symmetric NxN Matrix
+    !! @param eigvec Array of eigenvectors of A
+    !! @param eigval Array of eigenvalues of A
+    subroutine jacobi(A, eigvec, eigval)
+
+        use libtt_common,  only: eye, diag
+        use libtt_logical, only: isSquare, isSymmetric, isDiagonal
+
+        real(kind=dp), dimension(:,:), intent(in) :: A
+        real(kind=dp), dimension(:,:)             :: eigvec
+        real(kind=dp), dimension(:)               :: eigval
+
+        real(kind=dp), dimension(:,:), allocatable :: B
+        integer                                    :: n
+        integer                                    :: i
+        real(kind=dp)                              :: pivot
+        real(kind=dp)                              :: max
+        real(kind=dp)                              :: theta
+        real(kind=dp)                              :: factor
+        real(kind=dp), dimension(:,:), allocatable :: Q
+        integer,       dimension(2)                :: idx
+        real(kind=dp), parameter                   :: pi = 4 * atan(1.0_8)
+        
+        if ((.NOT.isSquare(A)) .OR. (.NOT.isSymmetric(A))) then
+            error stop "ERROR in JACOBI EIGENVALUE: Matrix is not square or symmetric!"
+        end if
+
+        n = size(A, 1)
+        
+        allocate(Q(n,n))
+        allocate(B(n,n))
+
+        B      = A
+        eigvec = eye(n)
+                
+        do while (.NOT. isDiagonal(B))
+            
+            pivot = 0.0
+            idx   = 0
+
+            ! Get maximum pivot element
+            do i = 2,n,1
+                max = maxval(abs(B(i, 1:(i-1))))
+                if (max > pivot) then 
+                    pivot  = maxval(abs(B(i, 1:(i-1))))
+                    idx(2) = maxloc(abs(B(i, 1:(i-1))), 1)
+                    idx(1) = i
+                end if 
+            end do
+
+            ! Construct rotational matrix Q
+            theta = 0.0
+            if (B(idx(1), idx(1)) /= B(idx(2), idx(2))) then
+                factor = 2 * B(idx(1), idx(2)) / (B(idx(2), idx(2)) - B(idx(1), idx(1)))
+                theta  = 0.5 * atan(factor)
+            else 
+                theta  = 0.25 * pi
+            end if
+            Q = eye(n) 
+            Q(idx(1), idx(1)) = cos(theta)
+            Q(idx(1), idx(2)) = sin(theta)
+            Q(idx(2), idx(2)) = cos(theta)
+            Q(idx(2), idx(1)) = -sin(theta)
+
+            ! Perform linearization step
+            eigvec = matmul(eigvec, Q)
+            B      = matmul(transpose(Q), matmul(B, Q))
+        end do
+
+        do i =1,n,1
+            eigval(i) = B(i,i)
+        end do
+
+        ! Clean up
+        deallocate(Q)
+        deallocate(B)
+
+    end subroutine jacobi
+
     
 end module libtt_solvers_eigenproblem
